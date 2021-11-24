1f71ea8bcc6643961e5c970c9cb37b9683812f1f
vaishali <vaishalinema91@gmail.com>
2021-11-24 14:39:44 +0530

    static

--- djangoresume/settings.py
+++ djangoresume/settings.py
@@ -129,8 +129,7 @@ STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'static'),
 ]
 
-STATICFILES_STORAGE = 
-'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())   
 
